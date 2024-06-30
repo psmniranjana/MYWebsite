3d2490eac9d87d2812894c9723971a0a6384c880
psmniranjana <psmniranjana@students.nsbm.ac.lk>
2024-06-30 12:25:44 +0530

    3rd  commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+console.log("malee"); //hidden subject
+
+    var studentAge = 23; 
+    var studentName = "Tharu";
+    var myMarks = 87;
+    console.log(studentAge);
+
+    //arithmatic ope
+
+    var val1 = 34;
+    var val2 = 16;
+
+    console.log(val1+val2);
+
+    //relational ope
+
+    console.log(val1>val2);
+
+    var isMarried = true //true, false
+
+    var mathsGrade = 'A';
+
+    console.log(isMarried);
+    console.log(mathsGrade);
+
+    //flow controllers
+
+    var age = 20;
+
+    //true, fale , or a statement that generate true or false
+    if(age>18){     
+        console.log("you are eligible");
+    }
+    else{
+        console.log("Call police");
+    }
